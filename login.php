a2d74b3a317a747779db43bb5a5d771efbe2447b
Joker Zhong <jiahaozhongjoker@gmail.com>
2021-12-11 15:23:47 -0500

    Add files via upload

---
+++ login.php
@@ -0,0 +1,21 @@
+<?php
+inculde('connection.php');
+$username = $POST['user'];
+$password = $POST['password'];
+
+$sql = "SELECT * FROM login WHERE (username = '$username') AND (password ='$password') limit 1";
+
+$result = $conn->query($sql);
+
+$row = mysqli_fetch_assoc($result);
+
+if($row > 0) {
+    $url = "login.html";
+
+    echo "<script>alert('Error. You should enter the valid username or password.')</script>";
+
+    echo "<meta http-equiv='Refresh' content='0; URL = $url'>";
+}
+
+$conn->close();
+?>
