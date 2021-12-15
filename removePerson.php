1b7e99825a455735b5e90cd9fb94def154d8674c
abajr516 <95147013+abajr516@users.noreply.github.com>
2021-12-15 01:26:40 -0500

    Update removePerson.php

--- removePerson.php
+++ removePerson.php
@@ -1,4 +1,21 @@
-<?php
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <title>Add/Remove People</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+  </head>
+
+  <body>
+    <header>
+      <h1>
+        CSIRT
+      </h1>
+      <h4>
+        Computer Security Incident Response Teams
+      </h4>
+    </header>
+    <?php
     include('connection.php');
     $personID = $_POST['personID'];
 
@@ -10,7 +27,7 @@
 
     if($conn->query($sql)){
 
-        echo '<p style="font-size:24pt;color:black;text-align:center">'."Submitted successfully! Returning to home page...".'<p>';
+        echo '<h2 style="font-size:24pt;color:black;text-align:center">'."Submitted successfully! Returning to home page...".'</h2>';
 
         $url = "index.html";
 
@@ -18,7 +35,7 @@
 
     }else{
 
-        echo '<p style="font-size:24pt;color:black;text-align:center">'."There was an error! Please resubmit.".'<p>';
+        echo '<h2 style="font-size:24pt;color:black;text-align:center">'."There was an error! Please resubmit.".'</h2>';
 
 
         $url1 = "addremovePeople.html";
@@ -30,3 +47,7 @@
 
         $conn->close();
 ?>
+    <h2>Powered by Runtime Errors</h2>
+  </body>
+
+</html>
