a9b8367db4268b2f758814734094b73be36d2c1b
Joker Zhong <jiahaozhongjoker@gmail.com>
2021-12-14 20:34:15 -0500

    Add files via upload

---
+++ addPerson.php
@@ -0,0 +1,36 @@
+<?php
+    include('connect.php');
+    $incidentId = $_POST['incidentId'];
+    $lastName = $_POST['lastName'];
+    $firstName = $_POST['firstName'];
+    $jobTitle = $_POST['jobTitle'];
+    $emailAddress = $_POST['emailAddress'];
+    $association = $_POST['association'];
+
+
+    $sql="INSERT INTO PEOPLE(incidentID,lastName,firstName,jobTitle,email, personAssociation)
+    VALUES('$incidentId','$lastName','$firstName','$jobTitle','$emailAddress','$association')";
+
+
+    if($conn->query($sql)){
+
+        echo '<p style="font-size:24pt;color:black;text-align:center">'."Submitted successfully! Returning to home page...".'<p>';
+
+        $url = "index.html";
+
+        echo "<meta http-equiv='Refresh' content='3;URL=$url'>";
+
+    }else{
+
+        echo '<p style="font-size:24pt;color:black;text-align:center">'."There was an error! Please resubmit.".'<p>';
+
+
+        $url1 = "addPerson.html";
+
+        echo "<meta http-equiv='Refresh' content='3;URL=$url1'>";
+
+
+    }
+
+        $conn->close();
+?>
