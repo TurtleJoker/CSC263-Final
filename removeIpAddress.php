370468469ec97448c7779c98767477855ebb0120
shiven1001 <95147295+shiven1001@users.noreply.github.com>
2021-12-14 23:36:19 -0500

    Add files via upload

---
+++ removeIpAddress.php
@@ -0,0 +1,30 @@
+<?php
+    include('connection.php');
+
+
+    $sql="DELETE INTO incidents_ipAddresses
+    WHERE ipAddress = ". $_POST['ipAddress'] ."; ";
+
+
+    if($conn->query($sql)){
+
+        echo '<p style="font-size:24pt;color:black;text-align:center">'."Submitted successfully! Returning to home page...".'<p>';
+
+        $url = "index.html";
+
+        echo "<meta http-equiv='Refresh' content='3;URL=$url'>";
+
+    }else{
+
+        echo '<p style="font-size:24pt;color:black;text-align:center">'."There was an error! Please resubmit.".'<p>';
+
+
+        $url1 = "addremoveipAddress.html";
+
+        echo "<meta http-equiv='Refresh' content='3;URL=$url1'>";
+
+
+    }
+
+        $conn->close();
+?>
